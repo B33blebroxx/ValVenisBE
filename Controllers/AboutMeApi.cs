821d5d9819bb28088b3fb24b823b007cfc18f204
B33blebroxx <b33blebroxx@gmail.com>
2024-08-05 13:21:36 -0500

    Added Get About Me and Update About Me in AboutMeApi controller

---
+++ Controllers/AboutMeApi.cs
@@ -0,0 +1,35 @@
+﻿using ValVenisBE.Models;
+
+namespace ValVenisBE.Controllers
+{
+    public class AboutMeApi
+    {
+        public static void Map(WebApplication app)
+        {
+
+            //Get AboutMe
+            app.MapGet("/aboutme", (ValVenisBEDbContext db) =>
+            {
+                var aboutme = db.AboutMes;
+                return Results.Ok(aboutme);
+            });
+
+            //Update AboutMe
+            app.MapPut("/aboutme/{id}", (ValVenisBEDbContext db, int id, AboutMe updatedAboutMe) =>
+            {
+                var aboutme = db.AboutMes.Find(id);
+                if (aboutme == null)
+                {
+                    return Results.NotFound();
+                }
+                aboutme.UserId = updatedAboutMe.UserId;
+                aboutme.AboutMeImage = updatedAboutMe.AboutMeImage;
+                aboutme.AboutMeProfileLink = updatedAboutMe.AboutMeProfileLink;
+                aboutme.AboutMeText = updatedAboutMe.AboutMeText;
+
+                db.SaveChanges();
+                return Results.Ok(aboutme);
+            });
+        }
+    }
+}
